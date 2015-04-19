c791515a5f5e7103294063b65505b22838039d2b
Enok Mikkelsen <enokmik@gmail.com>
2015-04-19 11:29:15 +0200

    added førSort, efterSort & sorterings method, green bar

--- 04-Implementation/src/romanAdder.java
+++ 04-Implementation/src/romanAdder.java
@@ -1,8 +1,10 @@
+import java.util.Arrays;
+
 
 public class romanAdder {
 
   public static String add(String venstre, String hojre) {
-    return normaliser(venstre + hojre);
+    return normaliser(sortering(venstre + hojre));
   }
   
   
@@ -17,4 +19,35 @@ public class romanAdder {
         ;
   }
   
+  private static String førSort (String førSort) {
+   return førSort
+       .replace( "M", "a" )
+       .replace( "D", "b" )
+       .replace( "C", "c" )
+       .replace( "L", "d" )
+       .replace( "V", "e" )
+       .replace( "X", "f" )
+       .replace( "I", "g" )
+       ;
+  }
+  
+  private static String efterSort (String førSort) {
+    return førSort
+        .replace( "a", "M" )
+        .replace( "b", "D" )
+        .replace( "c", "C" )
+        .replace( "d", "L" )
+        .replace( "e", "E" )
+        .replace( "f", "X" )
+        .replace( "g", "I" )
+        ;
+   }
+  
+  private static String sortering(String sort) {
+    førSort(sort);
+    char[] chars = sort.toCharArray();
+    Arrays.sort(chars);
+    String sorted = new String(chars);
+    return(efterSort(sorted));
+  }
 }
