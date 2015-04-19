1904723bd1126bc4417361b8ee5aa044fa7f26e4
Enok Mikkelsen <enokmik@gmail.com>
2015-04-19 10:51:35 +0200

    Added the test cases to the Test folder too Added 3th test case, red bar

--- 04-Implementation/src/romanAdderTest.java
+++ 04-Implementation/src/romanAdderTest.java
@@ -9,7 +9,9 @@ public class romanAdderTest {
   public void test() {
     // assertEquals("III", romanAdder.add("I","II"));
     
-    assertEquals("V", romanAdder.add("IIII","I"));
+    // assertEquals("V", romanAdder.add("IIII","I"));
+    
+    assertEquals("X", romanAdder.add("V","V"));
   }
 
 }
